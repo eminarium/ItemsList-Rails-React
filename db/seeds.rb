3350211e1844bf2322b96db2286c0969b756d3f4
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-09-25 18:46:39 +0500

    Created 10 elements with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times { |i|
+    Item.create!(name: 'Item #'+i.to_s, description: 'It is a description of item #'+i.to_s)
+}
